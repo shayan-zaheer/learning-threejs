cd969040119ec5c5559882a7580e8f9cfba7d332
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2024-09-26 05:42:26 +0500

    added grouping

--- 1 - Intro/index.js
+++ 1 - Intro/index.js
@@ -1,4 +1,36 @@
 const scene = new THREE.Scene(); // first, we create a scene, a scene can be referred to as the whole world of 3d
+
+const group = new THREE.Group();
+scene.add(group);
+
+/* 
+*/
+
+const box1 = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 2, 2),
+    new THREE.MeshBasicMaterial({color: 0xff000})
+);
+
+box1.rotation.x = Math.PI * 0.25;
+group.add(box1);
+
+const box2 = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 2, 2),
+    new THREE.MeshBasicMaterial({color: 0x000ff})
+);
+
+box2.position.x = -3;
+group.add(box2);
+
+const box3 = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 2, 2),
+    new THREE.MeshBasicMaterial({color: "orange"})
+);
+
+box3.position.x = 3;
+group.add(box3);
+/*
+
 const geometry = new THREE.BoxGeometry(2, 2, 2); // geometry can be said as any shape that we want to add, its parameters are (width, height, depth)
 const material = new THREE.MeshBasicMaterial({
     color: "red",
@@ -24,18 +56,24 @@ scene.add(box); // we add the box into the scene
 console.log(box.position.length()); // property to find the distance of the box from the center
 
 // box.position.normalize(); // to normalize any distorted object
+*/
 
 const camera = new THREE.PerspectiveCamera(75, 700 / 500); // camera is our viewangle
 camera.position.z = 5; 
 
 // camera.position.set(1, 1, 3);
 
+// camera.lookAt(new THREE.Vector3(1, 0, 1));
+// camera.lookAt(box.position); // thing is that camera always looks at the center, so we can assign a new vector or box's position so that it always follows the box
+
 scene.add(camera); // we add the camera to the scene
+
+
 
 const axesHelper = new THREE.AxesHelper();
 scene.add(axesHelper); // It shows the axes on to the scene, with RGB representing x,y,z axes.
 
-console.log(box.position.distanceTo(camera.position)) // property to find the distance of the box from any thing basically, here we're finding the distance of the box from the camera
+// console.log(box.position.distanceTo(camera.position)) // property to find the distance of the box from any thing basically, here we're finding the distance of the box from the camera
 
 const canvas = document.querySelector(".wbgl"); // we create a canvas in html and refer to that
 const renderer = new THREE.WebGLRenderer({ canvas }); // we render on to the canvas
